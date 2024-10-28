425c5f31aef4f5063932f45752586ea1af3e5f43
Lil127moone <lil127moone@gmail.com>
2024-10-28 19:49:29 +0200

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -5,10 +5,13 @@ import FreeSimpleGUI as sg
 label = sg.Text("Write to-dos")
 input_box = sg.InputText(tooltip="Enter the to-do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-do app",
-                   layout=[[label],[input_box, add_button]],
-                   font=("Helvetica", 20))
+                   layout=[[label],[input_box, add_button],
+                           [list_box, edit_button]],
+                   font=("Helvetica", 10))
 while True:
     event, values = window.read()
     print(event)
@@ -19,6 +22,23 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+
         case sg.WIN_CLOSED:
             break
 
