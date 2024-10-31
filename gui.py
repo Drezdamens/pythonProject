b876f134c6159164a583a11c1feb088f895f7574
Lil127moone <lil127moone@gmail.com>
2024-10-31 16:20:11 +0200

    Implement complete and exit buttons

--- gui.py
+++ gui.py
@@ -5,12 +5,15 @@ import FreeSimpleGUI as sg
 label = sg.Text("Write to-dos")
 input_box = sg.InputText(tooltip="Enter the to-do", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-do app",
                    layout=[[label],[input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 10))
 while True:
     event, values = window.read()
@@ -35,10 +38,16 @@ while True:
             functions.write_todos(todos)
 
             window["todos"].update(values=todos)
-
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
-
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
